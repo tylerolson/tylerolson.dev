97bc204f9a7aa71ac0950f99ad04dab88bef3c2d
Tyler Olson <tolson519@gmail.com>
2026-03-11 00:12:25 -0700

    frick me I guess

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -1,13 +1,8 @@
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
-
 export async function GET() {
-	const file = await readFile(resolve('static/Tyler_Olson_Resume.pdf'));
-
-	return new Response(file, {
+	return new Response(null, {
+		status: 302,
 		headers: {
-			'Content-Type': 'application/pdf',
-			'Content-Disposition': 'inline; filename="Tyler_Olson_Resume.pdf"'
+			Location: '/Tyler_Olson_Resume.pdf'
 		}
 	});
 }
